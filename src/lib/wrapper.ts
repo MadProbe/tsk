618adfd08aefe3696a4f32e9c15797f60be6aff9
MadProbe <49519179+MadProbe@users.noreply.github.com>
2021-01-12 00:27:56 +0300

    p => iife

--- src/lib/wrapper.ts
+++ src/lib/wrapper.ts
@@ -1,11 +1,11 @@
 var __async = `function __async(f,t,a){` +
     `return new p(function(r){` +
     `var g=__apply(f,t,a),n=g.next;` +
-    `_();` +
     `function _(p){` +
     `var v=__call(n,g,p);` +
     `v.done?r(v.value):pr(v.value).then(function(v){_(v)})` +
     `}` +
+    `_();` +
     `})` +
     `}`;
 
@@ -18,9 +18,9 @@ var __contains = `function __contains(i,v){` + // i = iterable, v = value
 var __throw = "function __throw(e){throw e}";
 
 export function wrap(code: string, used: Record<string, boolean>) {
-    return `(function(u){var p=Promise,c=n.call,__bind=c.bind(n.bind),` +
+    return `(function(p,u){var c=n.call,__bind=c.bind(n.bind),` +
         `pr=__bind(p.resolve,p),__call=__bind(c,c),__apply=__bind(c,n.apply);` +
         `${ __nullish }${ __async }${ used.contains ? __contains : "" }${ used.na ? __null_assert : "" }` +
         `${ used.throw ? __throw : "" }` +
-        `return ${ code }})()()`;
+        `return ${ code }})(Promise)()`;
 }
